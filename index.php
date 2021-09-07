020afcbee30e5c4ec7986005248746b9a50857ef
Mette <mette.ghijsen@gmail.com>
2021-09-07 23:34:29 +0200

    oei oei

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Portfolio Mette Ghijsen</title>
+</head>
+
+<body>
+<div class="inhoud">
+<ul class="vakken">
+    <li><a href="vakken/interactivemedia.php">Interactive Media</a></li>
+    <li><a href="vakken/development.php">Development</a></li>
+    <li><a href="vakken/design.php">Design</a></li>
+    <li><a href="vakken/research.php">Research</a></li>
+    <li><a href="vakken/communication.php">Communication</a></li>
+</ul>
+</div>
+
+<footer>
+    <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+</footer>
+</body>
+</html>

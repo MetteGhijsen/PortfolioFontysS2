127ca6447da8fbbd8b83a89e94a218d1037e14a4
I481695 <mette.ghijsen@student.fontys.nl>
2021-09-08 16:26:50 +0200

    gewerkt aan de projectpagina

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/home.css">
     <title>Portfolio Mette Ghijsen</title>
 </head>
 
@@ -17,11 +17,18 @@
     <li><a href="vakken/design.php">Design</a></li>
     <li><a href="vakken/research.php">Research</a></li>
     <li><a href="vakken/communication.php">Communication</a></li>
+    <li><a href="vakken/professionalidentity">Professional Identity</a></li>
 </ul>
 </div>
 
+<div class="logo">
+    <img src="img/logotje.png" alt="logo">
+</div>
+
 <footer>
+    <div class="copyrightnote">
     <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+    </div>
 </footer>
 </body>
 </html>
--- vakken/interactivemedia.php
+++ vakken/interactivemedia.php
@@ -5,9 +5,37 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/vakken.css">
     <title>Portfolio Mette Ghijsen</title>
 </head>
+
 <body>
-<li><a href="../index.php">Home</a></li>
+<div class="taakbalk">
+    <li><a href="../index.php">Home</a></li>
+</div>
+
+<div class="hoofdtekst">
+    <p>Interactive Media</p>
+</div>
+
+<div class="project-group">
+    <div class="afbeelding">
+        <img src="https://i.pinimg.com/originals/92/17/a8/9217a86945b10b32d03b1ae913d4af14.jpg" alt="pipi">
+    </div>
+    <div class="text">
+        <div class="titel">
+            <p> mijn kekke project</p>
+        </div>
+
+        <div class="artikel">
+            <p> heel boeiend enzo</p>
+        </div>
+
+        <div class="knop">
+            <button type="button">Click Me!</button>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>

1efa8e662aede21e8f0f37efe9a0e77480458330
I481695 <mette.ghijsen@student.fontys.nl>
2021-09-10 14:48:02 +0200

    een homepage gemaakt en heel de leerdoelen pagina geredesignd

--- leeruitkomsten.php
+++ leeruitkomsten.php
@@ -5,40 +5,57 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/leeruitkomsten.css">
+    <link rel="icon" href="img/logotjewit.png">
     <title>Portfolio Mette Ghijsen</title>
 </head>
 
 <body>
-<div class="taakbalk">
-    <li><a href="../index.php">Home</a></li>
-    <li class="pijltje"> <</li>
-    <li><a href="../leeruitkomsten.php" class="currentpage">Leeruitkomsten</a></li>
-</div>
+<div class="container">
+    <div class="taakbalk">
+        <li><a href="index.php">Home </a></li>
+        <li class="pijltje">< </li>
+        <li><a href="leeruitkomsten.php" class="currentpage">Leeruitkomsten</a></li>
+    </div>
+    <div class="hoofdtekst">
+        <p>Leeruitkomsten</p>
+    </div>
 
-<div class="hoofdtekst">
-    <p>Leeruitkomsten</p>
-</div>
-
-<div class="inhoud">
-    <ul class="vakken">
-        <li><a href="vakken/interactivemedia.php">Interactive Media</a></li>
-        <li><a href="vakken/development.php">Development</a></li>
-        <li><a href="vakken/design.php">Design</a></li>
-        <li><a href="vakken/research.php">Research</a></li>
-        <li><a href="vakken/communication.php">Communication</a></li>
-        <li><a href="vakken/professionalidentity">Professional Identity</a></li>
-    </ul>
-</div>
-
-
-<div class="logo">
-    <img src="img/logotje.png" alt="logo">
-</div>
-
-<div class="copyrightnote">
-    <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
-</div>
-
+<!--   <div class="inhoud">
+        <ul class="vakken">
+            <li><a href="vakken/interactivemedia.php">Interactive Media</a></li>
+            <li><a href="vakken/development.php">Development</a></li>
+            <li><a href="vakken/design.php">Design</a></li>
+            <li><a href="vakken/research.php">Research</a></li>
+            <li><a href="vakken/communication.php">Communication</a></li>
+            <li><a href="vakken/professionalidentity">Professional Identity</a></li>
+        </ul>
+    </div>-->
+    <div class="grid-container">
+        <div class="interactivemedia">
+            <li><a href="vakken/interactivemedia.php">1</a></li>
+        </div>
+        <div class="development">
+            <li><a href="vakken/development.php">2</a></li>
+        </div>
+        <div class="design">
+            <li><a href="vakken/design.php">3</a></li>
+        </div>
+        <div class="research">
+            <li><a href="vakken/research.php">4</a></li>
+        </div>
+        <div class="communication">
+            <li><a href="vakken/communication.php">5</a></li>
+        </div>
+        <div class="professionalidentity">
+            <li><a href="vakken/professionalidentity">6</a></li>
+        </div>
+    </div>
 </body>
+
+<footer>
+    <div class="copyrightnote">
+        <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+    </div>
+</footer>
 </html>

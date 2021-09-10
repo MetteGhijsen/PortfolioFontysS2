636b91ee7a552009a31dca8025f118c9be8263d7
I481695 <mette.ghijsen@student.fontys.nl>
2021-09-11 01:11:10 +0200

    nieuwe index hudts

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="icon" href="img/logotjewit.png">
+    <title>Portfolio Mette Ghijsen</title>
+</head>
+<body>
+<div class="container">
+    <div class="naam">
+        <p>Mette Ghijsen</p>
+    </div>
+    <div class="leeruitkomsten">
+        <li><a href="leeruitkomsten.php">Leeruitkomsten →</a></li>
+    </div>
+</div>
+</body>
+<footer>
+    <div class="copyrightnote">
+        <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+    </div>
+</footer>
+</html>

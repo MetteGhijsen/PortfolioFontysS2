25c05033c5424bed2e64af5782b12a0b94300783
I481695 <mette.ghijsen@student.fontys.nl>
2021-09-13 10:43:19 +0200

    projectpagina werkend gekregen en copyrightnotes toegevoegd aan alle pagina's

--- projects/interactive media projects/portfolioproject.php
+++ projects/interactive media projects/portfolioproject.php
@@ -5,21 +5,33 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/vakken.css">
+    <link rel="stylesheet" href="../../css/project.css">
+    <link rel="icon" href="../../img/logotjewit.png">
     <title>Portfolio Mette Ghijsen</title>
 </head>
 
 <body>
-<div class="taakbalk">
-    <li><a href="../index.php">Home</a></li>
-    <li class="pijltje"> < </li>
-    <li><a href="../leeruitkomsten.php">Leeruitkomsten</a></li>
-    <li class="pijltje"> < </li>
-    <li><a href="../vakken/interactivemedia.php">Interactive Media</a></li>
-    <li class="pijltje"> < </li>
-    <li><a href="../index.php" class="currentpage">Portfolio</a></li>
-</div>
+<div class="container">
+    <div class="taakbalk">
+        <li><a href="../../index.php"> Home </a></li>
+        <li class="pijltje"> < </li>
+        <li><a href="../../leeruitkomsten.php"> Leeruitkomsten </a></li>
+        <li class="pijltje"> < </li>
+        <li><a href="../../vakken/interactivemedia.php"> Interactive Media </a></li>
+        <li class="pijltje"> < </li>
+        <li><a href="../../projects/interactive%20media%20projects/portfolioproject.php" class="currentpage">Portfolio Website</a></li>
+    </div>
 
-<div class="hoofdtekst">
-    <p>Interactive Media</p>
+    <div class="hoofdtekst">
+        <p>Portfolio Website</p>
+    </div>
+
 </div>
+</body>
+
+<footer>
+    <div class="copyrightnote">
+        <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+    </div>
+</footer>
+</html>
--- vakken/interactivemedia.php
+++ vakken/interactivemedia.php
@@ -7,39 +7,49 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/vakken.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="icon" href="img/logotjewit.png">
+    <link rel="icon" href="../img/logotjewit.png">
     <title>Portfolio Mette Ghijsen</title>
 </head>
 
 <body>
-<div class="taakbalk">
-    <li><a href="../index.php">Home </a></li>
-    <li class="pijltje">< </li>
-    <li><a href="../leeruitkomsten.php">Leeruitkomsten </a></li>
-    <li class="pijltje">< </li>
-    <li><a href="../vakken/interactivemedia.php" class="currentpage"> Interactive Media</a></li>
-</div>
-
-<div class="hoofdtekst">
-    <p>Interactive Media</p>
-</div>
-
-<div class="project-group">
-    <div class="afbeelding">
-        <img src="https://cdn.statically.io/img/i.pinimg.com/originals/16/38/c4/1638c4515526ecdad29f0e43860ba45b.gif" alt="pipi">
+<div class="container">
+    <div class="taakbalk">
+        <li><a href="../index.php">Home </a></li>
+        <li class="pijltje"><</li>
+        <li><a href="../leeruitkomsten.php">Leeruitkomsten </a></li>
+        <li class="pijltje"><</li>
+        <li><a href="../vakken/interactivemedia.php" class="currentpage"> Interactive Media</a></li>
     </div>
-    <div class="text">
-        <div class="titel">
-            <p> Portfolio Website</p>
-        </div>
 
-        <div class="artikel">
-            <p> Dit is een portfolio-website die ik gemaakt heb gedurende dit semester. Ik wilde graag kijken of ik vanuit scratch een mooie, functionele website kon maken.</p>
-        </div>
+    <div class="hoofdtekst">
+        <p>Interactive Media</p>
+    </div>
 
-        <a href="../projects/interactive%20media%20projects/portfolioproject.php" class="btn"> <i class="bi bi-play"></i> Bekijk Project </a>
+    <div class="project-group">
+        <div class="afbeelding">
+            <img src="https://cdn.statically.io/img/i.pinimg.com/originals/16/38/c4/1638c4515526ecdad29f0e43860ba45b.gif"
+                 alt="pipi">
+        </div>
+        <div class="text">
+            <div class="titel">
+                <p> Portfolio Website</p>
+            </div>
+
+            <div class="artikel">
+                <p> Dit is een portfolio-website die ik gemaakt heb gedurende dit semester. Ik wilde graag kijken of ik
+                    vanuit scratch een mooie, functionele website kon maken.</p>
+            </div>
+
+            <a href="../projects/interactive%20media%20projects/portfolioproject.php" class="btn"> <i
+                        class="bi bi-play"></i> Bekijk Project </a>
+        </div>
     </div>
 </div>
 
 </body>
+<footer>
+    <div class="copyrightnote">
+        <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+    </div>
+</footer>
 </html>

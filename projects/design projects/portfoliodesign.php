b2de3f0e478a7a4b98f2a553ae4bf05a3a9fd80c
Mette <mette.ghijsen@gmail.com>
2021-12-13 14:09:57 +0100

    portfolio design toegevoegd aan website

---
+++ projects/design projects/portfoliodesign.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/portfoliodesign.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="icon" href="../../img/logotjewit.png">
+    <title>Portfolio Mette Ghijsen</title>
+</head>
+
+<body>
+<div class="container">
+    <div class="taakbalk">
+        <li><a href="../../index.php"> Home </a></li>
+        <li class="pijltje"> <</li>
+        <li><a href="../../leeruitkomsten.php"> Leeruitkomsten </a></li>
+        <li class="pijltje"> <</li>
+        <li><a href="../../vakken/design.php"> Design </a></li>
+        <li class="pijltje"> <</li>
+        <li><a href="../design%20projects/portfoliodesign.php" class="currentpage"> Portfolio Website Design </a></li>
+    </div>
+
+    <div class="hoofdtekst">
+        <p> Portfolio Website Design </p>
+    </div>
+
+    <div class="projectinfo">
+        <p><i class="bi bi-pencil"></i> Design</p>
+        <p><i class="bi bi-person-check-fill"></i> Individueel</p>
+    </div>
+
+    <div class="grid-container">
+        <div class="image">
+            <img src="../../img/design/portfolioachtergrondV1.png" alt="projectafbeelding">
+        </div>
+        <div class="image2">
+            <img src="../../img/design/portfolioachtergrondV4.png" alt="projectafbeelding">
+            <img src="../../img/design/portfolioontwerp.PNG" alt="projectafbeelding" style="height: 87%">
+        </div>
+        <div class="text">
+            <h1>Inleiding</h1>
+            <p>Om onze voortgang dit semester bij te houden moesten we een portfoliowebsite maken waar we ons werk op
+                konden publiceren. In dit deel leg ik uit wat ik heb gedaan om tot het design te komen waar ik nu
+                ben. </p>
+        </div>
+        <div class="text2">
+            <h1>Het designproces</h1>
+            <p>Als eerste ging ik naar wat inspiratie zoeken. Ik wilde graag een mooi en uniek design neerzetten. Ik heb
+                op Behance gekeken en stuitte al gauw op
+                <a href="https://www.behance.net/gallery/121045079/PORTFOLIO-2021?tracking_source=search_projects_recommended%7Cportfolio"
+                   style="color: #0900b6" target="_blank">dit
+                    portfolio</a>. Ik vond dit erg vet, en ik probeerde al gauw iets soortgelijks te maken. Omdat ik in
+                de eerste paar weken van dit semester nog niet wist dat Adobe XD bestond, heb ik het ontwerp van mijn
+                website toen gemaakt in Adobe Photoshop. Hier rechts staat een van de ontwerpen die ik had gemaakt naar aanleiding
+                van het portfolio dat ik op Behance had gevonden. <br><br> Ik vond toch dat het ontwerp wat miste. Ik had het
+                gevoel dat ik iemand aan het namaken was in plaats van dat ik mijn eigen creatieve draai gaf aan het al
+                bestaande ontwerp. Ik heb geëxperimenteerd met de kleuren, en de teksten. Vervolgens heb ik wat
+                leeruitkomsten toegevoegd. Toen zag het er als volgt uit. <br><br>
+                Ik vond dit design nog steeds niet helemaal passen bij mij. Daarnaast was ik ook nog te onervaren met
+                HTML/CSS om dit werkelijkheid te maken en er echt goed uit te laten zien. Ik wist ook niet zo goed hoe
+                ik verder zou moeten met de site na deze pagina. Ik vond het heel lastig, en ik wist ook dat ik binnen 1
+                week al een site staande moest hebben. Dus ik had ook niet het gevoel dat ik mijn tijd kon nemen met het
+                designproces. Daarom besloot ik voor mijn eerste portfoliowebsite aan de slag te gaan met een iets
+                simpeler ontwerp dat ook te realiseren was. Ik heb het hele ontwerp weggegooid en ben opnieuw begonnen.
+                <br><br>
+                Voor het maken van een nieuw ontwerp, heb ik inspiratie gezocht bij medeleerlingen, aangezien de opzet van ons portfolio er iets anders
+            uitziet dat portfolio's van professionals. Wij moeten onze content indelen op basis van onze leeruitkomsten, en
+            ik was nieuwsgierig hoe mijn medeleerlingen dit deden. Al gauw stuitte ik op <a
+                        href="https://i433728.venus.fhict.nl/" style="color: #0900b6" target="_blank">de website van Luc Swinkels</a>. <br>Ik
+            zag dat hij tegeltjes gebruikte als knoppen voor de leeruitkomsten. Ik ben hiermee gaan spelen,
+            en uiteindelijk kwam ik tot het design dat ik nu heb.</p>
+        </div>
+        <div class="text5">
+            <h1>Wat heb ik geleerd?</h1>
+            <p>Dit is de eerste website die ik ooit heb gemaakt, dus het was een behoorlijk leerproces. Er zijn veel dingen die ik nu anders zou doen. Maar door mijn gebrek aan kennis in HTML/CSS/Javascript aan het begin van het semester heb ik sommige dingen redelijk onhandig aangepakt. Wat HTML en CSS betreft heb ik veel geleerd over flexboxen en grids. Ik heb ook geleerd hoe ik een gebruiksvriendelijke layout maak, en hoe ik feedback van verschillende mensen kan verwerken zodat de website optimaal werkt voor iedereen. Ik heb ook geleerd dat je bij je eigen waarden moet blijven, en niet altijd naar iedereens feedback hoeft te luisteren. Ik denk dat het belangrijk is om hier een mooi balans tussen te vinden.</p>
+        </div>
+        <div class="text4">
+            <h1>Feedback</h1>
+            <p> Op het design van mijn portfolio heb ik feedback ontvangen van meerdere docenten. Onder anderen van
+                Woody, Jo-Ann, Frank en Bram.
+                <br><br>Jo-Ann vond mijn website een hele mooie professionele uitstraling hebben.
+                De website zag er volgens haar uit alsof deze gemaakt was door iemand die echt wist wat hij deed.
+                <br><br> Woody had eerst enige twijfels over lettertype dat ik gebruikte, waarna ik dit veranderd heb.
+                Daarnaast vond hij de breadcrumbs ook niet mooi. Verder was hij wel erg tevreden over het design, en hij
+                vond dat de animaties een waardevolle toevoeging waren aan de site.
+                <br><br>
+                Bram vond de tegeltjes die ik gebruikte als knoppen voor de leeruitkomsten niet zo mooi. Verder zei hij ook dat
+                ik mijn website best vol propte. Hij legde uit dat het overlaten van ruimte rust kan creeëren in het hoofd
+                van de gebruiker. Dit is zeker iets waar ik naar ga kijken en wat ik anders ga doen bij mijn volgende projecten.
+                Tot slot was hij wel van mening dat ik een goed oog had voor design, en dat mijn website een mooie en moderne uitstraling had.
+            </p>
+        </div>
+    </div>
+    <div class="github">
+        <h2>Klik hier om het project te bekijken op Github</h2>
+        <a href="https://github.com/metteghijsen/PortfolioFontysS2" target="_blank">
+            <img src="../../img/github.png" alt="githublogo">
+        </a>
+    </div>
+</body>
+
+<footer>
+    <div class="copyrightnote">
+        <p>Copyright ©2021 By Mette Ghijsen, All Rights Reserved</p>
+    </div>
+</footer>
+</html>
+
+
+

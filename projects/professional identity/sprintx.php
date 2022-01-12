e6808ce971b2b71d62b3addc85eb90542def5de3
I481695 <mette.ghijsen@student.fontys.nl>
2022-01-12 11:13:59 +0100

    feedback sprint x toegevoegd

--- projects/professional identity/sprintx.php
+++ projects/professional identity/sprintx.php
@@ -55,8 +55,8 @@
             <p>
                 Nadat ik goedkeuring van Bram had gekregen voor het maken van een animatie, ging ik kijken naar welke software ik het beste kon gebruiken. Ik kwam tot de conclusie dat ik graag gebruik wilde maken van Adobe Animate. Ik had al de complete Creative Cloud, en ik wist van een aantal animators dat ze deze software ook gebruikten. <br><br>
 
-                Mijn eerste week bestond vooral uit het bekend worden met Adobe Animate. Ik heb een paar basistutorials gevolgd over de werking van het programma. Een aantal van Animate zelf, maar ook een aantal van YouTube. Ik merkte dat ik de tutorials van YouTube veel fijner vond, omdat ik daar ook daadwerkelijk zelf aan het tekenen was, in plaats van dat ik vooraf gemaakte assets gebruikte van Adobe. Nadat ik bekend was met de werking van het programma, besloot ik nog wat onderzoek te doen naar animatie. Hoe animeer je iemand die loopt? Hoe animeer je zwaartekracht? Hoe animeer je water? Voor elk van deze dingen gebruik je andere technieken. Daarnaast heb ik ook nog onderzoek gedaan naar framerate e.d. <br>
-                Voor mijn eindanimatie heb ik besloten “On two’s” (elke twee frames) te animeren, in 24 frames per seconde. Echter kwam ik er soms achter dat ik voor snelle bewegingen beter elke frame kon animeren. Dit gaf ook een vloeierende animatie.
+                Mijn eerste week bestond vooral uit het bekend worden met Adobe Animate. Ik heb een paar basistutorials gevolgd over de werking van het programma. Een aantal van Animate zelf, maar ook een aantal van YouTube. Ik merkte dat ik de tutorials van YouTube veel fijner vond, omdat ik daar ook daadwerkelijk zelf aan het tekenen was, in plaats van dat ik vooraf gemaakte assets gebruikte van Adobe. Nadat ik bekend was met de werking van het programma, besloot ik nog wat onderzoek te doen naar animatie. Hoe animeer je iemand die loopt? Hoe animeer je zwaartekracht? Hoe animeer je water? Hoe werkt union skinning? Daarnaast heb ik ook nog onderzoek gedaan naar framerate e.d. <br>
+                Voor mijn eindanimatie heb ik besloten “On twos” (elke twee frames) te animeren, in 24 frames per seconde. Echter kwam ik er soms achter dat ik voor snelle bewegingen beter elke frame kon animeren. Dit gaf ook een vloeierende animatie.
                 <br><br>
             </p>
             <h1>Week 2</h1>
@@ -70,10 +70,21 @@
         <div class="text3">
             <h1>Wat heb ik geleerd?</h1>
             <p>Ik heb best wel veel geleerd van deze sprint. Als eerste heb ik geleerd hoe ik moet werken met Adobe Animate. Uiteraard beheers ik geen hoog niveau van Animate, maar wanneer ik in de toekomst verder wil gaan, of simpele animaties wil maken voor op websites is dit wel een mooie basis om op verder te bouwen. Daarnaast heb ik wat kennis opgedaan over animeren. Specifiek weet ik nu hoe het werkt met animeren in frames. Wat is de aanbevolen framerate, en op watvoor manieren kun je de frames invullen? Ook heb ik geleerd hoe je frame bij frame animaties tekent. Je kan bijvoorbeeld animeren door afbeeldingen te vervormen, maar ik heb gebruik gemaakt van mijn tekentablet om zo elke frame zelf te tekenen. </p>
+        <h1>Wat zou ik anders doen?</h1>
+            <p>Met de kennis die ik heb opgedaan zijn er wel dingen die ik anders zou doen. Ik ben namelijk toch niet helemaal tevreden met de kwaliteit van mijn eindanimatie. Ik zou de kwaliteit kunnen verbeteren door een iets rustiger en consistenter kleurenpalet aan te houden. De video is best wel fel nu. Daarnaast zou ondertiteling of een diepgaander script de kwaliteit van de video ook wel goed doen. Als laatste zou ik on ones animeren in plaats van on twos. Ik heb nu alleen on twos geanimeerd door een gebrek aan tijd. Maar denk dat de animatie er een stuk beter en vloeiender uit zou zien als ik gewoon on ones had geanimeerd.</p>
         </div>
         <div class="text4">
             <h1>Feedback</h1>
-            <p> Ik moet nog feedback krijgen van Bram</p>
+            <p>
+                Bram Tuns (mijn semestercoach) vindt dat ik een duidelijke ontwikkeling hebt laten zien. Hij vindt het goed dat ik eerst een tutorial heb gedaan om de software te leren kennen. Het doen van die tutorial staat ook in dienst van een hoger doel. Namelijk mijn eindanimatie en wat ik daarmee wil overbrengen. Hij verwacht daarbij wel dat ik ook kritisch terug kan kijken naar mijn eigen werk. En is van mening dat ik dat ook wel kan.</p>
+            Verder heb ik ook nog tijdens de video presentatie feedback ontvangen van verschillende docenten.
+            <br><br>
+            “Het ziet er goed uit, de boom die groeit, dat aan het eind er blaadjes in staan. De lamp die uitgaat, het stromende water. Goed over nagedacht. De ‘Hey jij’ is misschien te druk, dat zou je nog kunnen verbeteren. Wanneer het wordt gebruikt als professioneel product zou ik misschien nog ondertiteling toevoegen.” – Paul Reekers
+            <br><br>
+            “Mooie basis gelegd, vooral leuk dat je milieu als onderwerp hebt genomen.” – Huub Prüst
+            <br><br>
+            “Ik zie dat het heel veel werk is. Vooral als je alles zelf tekent d.m.v union skinning. Het kan zeker wel een professioneel product zijn.  Wel wat meer richting jongere kinderen misschien. Voor een oudere doelgroep zou ik misschien de stijl aanpassen.” – Joris Ariëns
+
         </div>
     </div>
     <div class="download">
